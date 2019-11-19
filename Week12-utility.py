273746326e0a376cf67d1abbc5ae2acd4ae76d36
Kamryn Reams <kamrynreams@mymail.mines.edu>
2019-11-18 17:42:23 -0700

    FindWordCount Function

--- Week12-utility.py
+++ Week12-utility.py
@@ -15,3 +15,11 @@ def UpdateString(string, character, index):
     list[index] = character
     updated_string = ''.join(list)
     print("OUTPUT", updated_string)
+
+
+def FindWordCount(list, string):
+    counter = 0
+    for i in range(len(list)):
+        if list[i] == string:
+            counter += 1
+    return(counter)
