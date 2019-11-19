29fef24e78bf5da4855cbfac916ad32ceebb1da6
Kamryn Reams <kamrynreams@mymail.mines.edu>
2019-11-18 17:44:26 -0700

    ScoreFinder Function

--- Week12-utility.py
+++ Week12-utility.py
@@ -23,3 +23,19 @@ def FindWordCount(list, string):
         if list[i] == string:
             counter += 1
     return(counter)
+
+
+def ScoreFinder(names_list, scores_list, player):
+    new_names_list = []
+    player_to_find = player.lower()
+    for name in names_list:
+        new_names_list.append(name.lower())
+    if player_to_find in new_names_list:
+        for i in range(len(new_names_list)):
+            if new_names_list[i] == player_to_find:
+                index = i
+        score = scores_list[index]
+        player = player_to_find.capitalize()
+        print("Output %s got a score of %d" %(player , score))
+    else:
+        print("OUTPUT player not found")
